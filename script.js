bc81d4455790d4c03c74e65433aeedf0d3eea616
kyle <kyletapac4@gmail.com>
2026-01-13 18:03:59 +0800

    update index

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-function showMessage() {
-    alert("Thank you for your interest! Let's work together 🚀");
+class PortfolioApp {
+    constructor() {
+        this.name = "Zean Kyle C. Tapac";
+        this.roles = [
+            "Future Software Developer",
+            "Web Developer",
+            "Hardware Enthusiast",
+            "IoT System Builder"
+        ];
+        this.nameIndex = 0;
+        this.roleIndex = 0;
+
+        this.typeName();
+        this.rotateRole();
+    }
+
+    typeName() {
+        if (this.nameIndex < this.name.length) {
+            document.getElementById("typing-name").textContent +=
+                this.name.charAt(this.nameIndex);
+            this.nameIndex++;
+            setTimeout(() => this.typeName(), 120);
+        }
+    }
+
+    rotateRole() {
+        document.getElementById("role").textContent =
+            this.roles[this.roleIndex];
+        this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+        setTimeout(() => this.rotateRole(), 2500);
+    }
+
+    toggleDark() {
+        document.body.classList.toggle("dark");
+    }
 }
+
+const app = new PortfolioApp();
